c55d9d0f8de151748f9920399582f1024b74e659
lafrippouille <48294571+lafrippouille@users.noreply.github.com>
2019-04-09 12:06:59 +0200

    Add files via upload

---
+++ monscript.js
@@ -0,0 +1,150 @@
+
+
+
+
+/*************Animation barres*****************/
+// Ajoute les classes pour les animations
+function BarAnimation() {
+  document.getElementById('progress_bar_html').classList.add('bar_html');
+  document.getElementById('progress_bar_ps').classList.add('bar_ps');
+  document.getElementById('progress_bar_pp').classList.add('bar_pp');
+  document.getElementById('progress_bar_bootstrap').classList.add('bar_bootstrap');
+  document.getElementById('progress_bar_cms').classList.add('bar_cms');
+}
+
+// Check si l'element passé en param est affiché sur l'écran
+function checkVisible(elm) {
+  var rect = elm.getBoundingClientRect();
+  var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+
+//Regarde si la div competence est affichée toutes les 250ms puis désactive l'interval
+var interval = setInterval(function() {
+    if ( checkVisible(document.getElementById('competences')))     {
+        BarAnimation();
+        clearInterval(interval); // Désactive le SetInterval
+    }
+}, 250);
+
+
+var n = 1500; // Nombre final du compteur
+var cpt = 0; // Initialisation du compteur
+var duree = 1; // Durée en seconde pendant laquel le compteur ira de 0 à 15
+var delta = Math.ceil((duree * 1) / n); // On calcule l'intervalle de temps entre chaque rafraîchissement du compteur (durée mise en milliseconde)
+var node =  document.getElementById("compteur"); // On récupère notre noeud où sera rafraîchi la valeur du compteur
+
+function countdown() {
+  node.innerHTML = ++cpt;
+  if( cpt < n ) { // Si on est pas arrivé à la valeur finale, on relance notre compteur une nouvelle fois
+     setTimeout(countdown, delta);
+  }
+}
+
+setTimeout(countdown, delta);
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 100 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+
+
+/*****************COMPTEUR**********************/
+$(function () {
+	$(window).on("scroll", function () {
+  	var shivaElement = $('#shiva'),
+    		span = shivaElement.find('.count'),
+    		fire = isOnScreen(span);
+
+    if(fire){
+      $({
+      	Counter: Number(span.text())
+      }).animate({
+        Counter: span.attr('data-total')
+      }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function () {
+          $(span).text(Math.ceil(this.Counter));
+        },
+      });
+    }
+  });
+});
+
+function isOnScreen(element) {
+  var viewPortHeight = $(window).height(), // Viewport Height
+      scrollTop = $(window).scrollTop(), // Scroll Top
+      currElementPosY = $(element).offset().top,
+      elementHeight = $(element).height();
+
+  return (currElementPosY + elementHeight > scrollTop && currElementPosY < (viewPortHeight + scrollTop))
+}
